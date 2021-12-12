4e926213cb5f1253cde271d56de681de73650a96
martin <martinbaliak2@hotmail.com>
2021-12-12 17:26:58 +0100

    Add navbar, features, home page, install dependencies

---
+++ netflix/src/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./home.scss";
+import Navbar from '../components/navbar/Navbar';
+import Featured from '../components/featured/Featured';
+
+const Home = () => {
+    return (
+        <div className='home'>
+            <Navbar/>
+            <Featured type="movie" />
+        </div>
+    )
+}
+
+export default Home
