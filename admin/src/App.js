6d67dd7bd53c7ca6912476053bf0c15c01022ef0
martin <martinbaliak2@hotmail.com>
2021-12-19 13:41:04 +0100

    add pages, create home page, featured component

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
+import Home from "./pages/home/Home";
 
 export default function App() {
   return (
@@ -8,7 +9,7 @@ export default function App() {
      <Topbar />
       <div className="container">
         <Sidebar />
-        <div className="others">other pages</div>
+        <Home />
       </div>
     </div>
   );
--- admin/src/components/sidebar/Sidebar.jsx
+++ admin/src/components/sidebar/Sidebar.jsx
@@ -3,43 +3,97 @@ import "./sidebar.css";
 import LineStyleIcon from '@mui/icons-material/LineStyle';
 import TimelineIcon from '@mui/icons-material/Timeline';
 import TrendingUpIcon from '@mui/icons-material/TrendingUp';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
+import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import DynamicFeedIcon from '@mui/icons-material/DynamicFeed';
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
+import ReportIcon from '@mui/icons-material/Report';
 
 export default function Sidebar() {
     return (
         <div className='sidebar'>
             <div className="sidebarWrapper">
+
                 <div className="sidebarMenu">
                     <h3 className='sidebarTitle'>Dashboard</h3>
                     <ul className='sidebarList'>
-                    <li className='sidebarListItem'>
-                        <LineStyleIcon />
+                    <li className='sidebarListItem active'>
+                        <LineStyleIcon className="sidebarIcon"/>
                         Home
                     </li>
                     <li className='sidebarListItem'>
-                        <TimelineIcon />
+                        <TimelineIcon className="sidebarIcon"/>
                         Analytics
                     </li>
                     <li className='sidebarListItem'>
-                        <TrendingUpIcon />
+                        <TrendingUpIcon className="sidebarIcon"/>
                         Sales
                     </li>
-                    </ul>
-                    <h3 className='sidebarTitle'>Dashboard</h3>
+                    </ul>    
+                </div>
+
+                <div className="sidebarMenu">
+                    <h3 className='sidebarTitle'>Quick Menu</h3>
                     <ul className='sidebarList'>
                     <li className='sidebarListItem'>
-                        <LineStyleIcon />
-                        Home
+                        <PersonOutlineIcon className="sidebarIcon"/>
+                        Users
                     </li>
                     <li className='sidebarListItem'>
-                        <TimelineIcon />
+                        <Inventory2OutlinedIcon className="sidebarIcon"/>
+                        Products
+                    </li>
+                    <li className='sidebarListItem'>
+                        <AttachMoneyOutlinedIcon className="sidebarIcon"/>
+                        Transactions
+                    </li>
+                    <li className='sidebarListItem'>
+                        <BarChartOutlinedIcon className="sidebarIcon"/>
+                        Reports
+                    </li>
+                    </ul>    
+                </div>
+
+                <div className="sidebarMenu">
+                    <h3 className='sidebarTitle'>Notifications</h3>
+                    <ul className='sidebarList'>
+                    <li className='sidebarListItem'>
+                        <MailOutlineIcon className="sidebarIcon"/>
+                        Mail
+                    </li>
+                    <li className='sidebarListItem'>
+                        <DynamicFeedIcon className="sidebarIcon"/>
+                        Feedback
+                    </li>
+                    <li className='sidebarListItem'>
+                        <ChatBubbleOutlineIcon className="sidebarIcon"/>
+                        Messages
+                    </li>
+                    </ul>    
+                </div>
+
+                <div className="sidebarMenu">
+                    <h3 className='sidebarTitle'>Staff</h3>
+                    <ul className='sidebarList'>
+                    <li className='sidebarListItem'>
+                        <WorkOutlineIcon className="sidebarIcon"/>
+                        Manage
+                    </li>
+                    <li className='sidebarListItem'>
+                        <TimelineIcon className="sidebarIcon"/>
                         Analytics
                     </li>
                     <li className='sidebarListItem'>
-                        <TrendingUpIcon />
-                        Sales
+                        <ReportIcon className="sidebarIcon"/>
+                        Reports
                     </li>
-                    </ul>
+                    </ul>    
                 </div>
+
             </div>
         </div>
     )
