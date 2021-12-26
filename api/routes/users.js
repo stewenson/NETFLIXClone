e65804707b437b09eee7314b90d267d0fecfd699
martin <martinbaliak2@hotmail.com>
2021-12-26 19:12:08 +0100

    crud user operation

--- api/routes/users.js
+++ api/routes/users.js
@@ -12,7 +12,7 @@ router.put("/:id", verify, async (req, res) => {
           process.env.SECRET_KEY
         ).toString();
       }
-      
+
       try {
         const updatedUser = await User.findByIdAndUpdate(
           req.params.id,
@@ -29,9 +29,49 @@ router.put("/:id", verify, async (req, res) => {
       res.status(403).json("You can update only your account!");
     }
   });
+
 //DELETE
+router.delete("/:id", verify, async (req, res) => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      try {
+        await User.findByIdAndDelete(req.params.id,);
+        res.status(200).json("User has been deleted");
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You cannot delete only your account!");
+    }
+  });
+
 //GET
+router.get("/find/:id", async (req, res) => {
+      try {
+        const user = await User.findById(req.params.id,);
+        const { password, ...info} = user._doc;
+        res.status(200).json(info);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  });
+
 //GET ALL
+router.get("/", verify, async (req, res) => {
+    const query = req.query.new;
+    if (req.user.isAdmin) {
+      try {
+        const users = query
+          ? await User.find().sort({ _id: -1 }).limit(2)
+          : await User.find();
+        res.status(200).json(users);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You are not allowed to see all users!");
+    }
+  });
+
 //GET USER STATS
 
 module.exports = router;
