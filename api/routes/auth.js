643e0d9daea5b695d871aeb427701440bd4e805c
martin <martinbaliak2@hotmail.com>
2021-12-26 17:32:32 +0100

    login api, tested by postman.

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,24 +3,48 @@ const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 
-//REGISTER
+/* 
+* REGISTER
+*/
 router.post("/register", async(req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
         password: CryptoJS.AES.encrypt(
-            req.body.password, 
-            process.env.SECRET_KEY
-        ).toString(),
-    });
+                req.body.password, 
+                process.env.SECRET_KEY
+                ).toString(),
+        });
 
-    try{
+    try {
         const user = await newUser.save();
         res.status(201).json(user)
-    }catch(err){
+    } catch (err) {
+        res.status(500).json(err);
+    }});
+
+/*
+* LOGIN
+*/
+router.post("/login", async(req, res) => {
+    try {
+        const user = await User.findOne({email: req.body.email})                    // try find user by email
+        !user && res.status(401).json("Wrong password or username!")
+        
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY); // encrypt password
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassword !== req.body.password &&                                   // compare password
+            res.status(401).json("Wrong password or username!")
+
+        const {password, ...info} = user._doc;                                      // Separate password from, password wont be visible in json
+
+    res.status(200).json(info)
+
+    } catch (err) {
         res.status(500).json(err);
     }
-    
-});
+})
+
 
 module.exports = router;
